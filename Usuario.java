cddd4abec15f4f94bd4d6729c84ac0d73aae4ec1
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2025-06-12 14:39:37 -0300

    Passos iniciais

---
+++ Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    private String nome;
+    private String userName;
+    private String senha;
+
+    public Usuario(String nome,String userName, String senha){
+        this.nome = nome;
+        this.userName = userName;
+        this.senha = senha;
+    }
+
+    
+}
