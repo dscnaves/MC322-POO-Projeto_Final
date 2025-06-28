b80f1c9581e7641a0674473246defef2c9ab6c58
dscnaves <dscnaves@gmail.com>
2025-06-28 17:39:53 -0300

    lalalala

--- Aluno.java
+++ Aluno.java
@@ -3,7 +3,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// hayuasdyfgyudsdasf
+// oie oie gente dsfafyufayuduydfys
 public class Aluno extends Usuario {
 
     private final ArrayList<Treino> treinosRecebidos;
