8eac57e858cf12a259e6c4f5e271e852d65ad4af
Ana-Luiza-Gomes <a242389@dac.unicamp.br>
2025-06-28 10:27:43 -0300

    teste de commit + comentários na classe usuário

--- Usuario.java
+++ Usuario.java
@@ -1,27 +1,33 @@
 import java.util.Scanner;
 
 public abstract class Usuario {
-    private String nome;
-    private String userName;
-    private String senha;
 
+    private final String nome;
+    private final String userName;
+    private final String senha;
+
+    // Construtor
     public Usuario(String nome,String userName, String senha){
         this.nome = nome;
         this.userName = userName;
         this.senha = senha;
     }
 
+    // Obtém o nome do usuário
     public String getNome() {
         return nome;
     }
     
+    // Obtém o username
     public String getUserName(){
         return userName;
     }
 
+    // Obtém a senha 
     public String getSenha(){
         return senha;
     }
 
-    public abstract void mostrarMenuPrincipal(Scanner entrada); // Metodo abstrato que implementaremos na classe aluno e professor.
+    // Método abstrato que implementaremos na classe aluno e professor
+    public abstract void mostrarMenuPrincipal(Scanner entrada);
 }
